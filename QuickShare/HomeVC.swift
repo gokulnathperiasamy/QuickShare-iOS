f90e9fd7c52737230b27306d0c466d58db99ba88
Gokul Nath KP <gokulnath.kpgn@gmail.com>
2017-05-04 22:35:33 +0530

    Implement about dialog.

--- QuickShare/HomeVC.swift
+++ QuickShare/HomeVC.swift
@@ -20,6 +20,11 @@ class HomeVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func aboutAction(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: Messages.ABOUT_DIALOG_TITLE, message: Messages.LINE_BREAK + Messages.ABOUT_DIALOG_MESSAGE + Messages.DOUBLE_LINE_BREAK + Messages.ABOUT_DIALOG_EMAIL_ID, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: Messages.ACTION_OK, style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
 
---
+++ QuickShare/Messages.swift
@@ -0,0 +1,23 @@
+//
+//  Messages.swift
+//  QuickShare
+//
+//  Created by Gokul Nath KP on 04/05/17.
+//  Copyright © 2017 Gokul Nath KP. All rights reserved.
+//
+
+import Foundation
+
+class Messages {
+    
+    public static let LINE_BREAK: String = "\n"
+    public static let DOUBLE_LINE_BREAK: String = "\n\n"
+    
+    public static let ACTION_OK: String = "OK"
+    public static let ACTION_CANCEL: String = "Cancel"
+    
+    public static let ABOUT_DIALOG_TITLE: String = "About"
+    public static let ABOUT_DIALOG_MESSAGE: String = "Copyright © 2017 Quick Share. All rights reserved."
+    public static let ABOUT_DIALOG_EMAIL_ID: String = "gokulnath.aws@gmail.com"
+
+}

edf07b8dbe97de8bba61be917ff3c1192b257f85
Gokul Nath KP <gokulnath.kpgn@gmail.com>
2017-05-04 22:18:32 +0530

    Initial Commit

---
+++ QuickShare/ProfileVC.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileVC.swift
+//  QuickShare
+//
+//  Created by Gokul Nath KP on 04/05/17.
+//  Copyright © 2017 Gokul Nath KP. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
